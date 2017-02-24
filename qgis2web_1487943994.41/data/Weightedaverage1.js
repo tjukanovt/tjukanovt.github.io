65747ca85fe9fdd719257313d9286434f4336791
tjukanovt <topi.tjukanov@gmail.com>
2017-02-24 16:01:29 +0200

    Changed background map

--- qgis2web_1487943994.41/data/Weightedaverage1.js
+++
@@ -1,7 +0,0 @@
-var json_Weightedaverage1={
-"type": "FeatureCollection",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "Average": null }, "geometry": { "type": "Point", "coordinates": [ 24.340238912447145, 61.1128651706181 ] } }
-]
-}
